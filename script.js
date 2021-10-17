19f1db86a5aa78b3fda47675ad1f42710ec0dde7
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-10-17 15:16:27 +0300

    -

--- script.js
+++ script.js
@@ -20,184 +20,170 @@ function popup() {
     });
 }
 document.addEventListener('DOMContentLoaded', () => {
-    let CanvasTask1 = document.querySelector('.canvas__task1');
-    let canvasTask1Img = CanvasTask1.getContext('2d');
-    canvasTask1Img.fillRect(50, 50, 50, 50);
-
-    //line
-    canvasTask1Img.beginPath();
-    canvasTask1Img.moveTo(100, 10);
-    canvasTask1Img.lineTo(200, 54);
-    canvasTask1Img.lineTo(200, 14);
-    canvasTask1Img.closePath(); //замкнуть
-    canvasTask1Img.stroke(); //конец рисования линии
-
-    canvasTask1Img.beginPath();
-    canvasTask1Img.moveTo(150,40);
-    canvasTask1Img.lineTo(150,100);
-    canvasTask1Img.lineTo(170, 150);
-    canvasTask1Img.fill(); //зарашивает-замыкает без closePath() и stroke()
-
-    canvasTask1Img.strokeRect(550, 150, 100, 75); //не закрашенный прямоугол
-    canvasTask1Img.fillRect(550, 10, 100, 75); //акрашенный прямоугол
-
-    //canvasTask1Img.rect(750, 150, 100, 75); //прмоугольник
-    //canvasTask1Img.stroke(); //отрисует контур прямоугольника
-    //canvasTask1Img.fill(); //закрасит прямоугольник
+    let clockFace = new ClockFace(10, 'white', 'red', 50, new CtxClock());
+    let hours = new Arrow(0.7, 16, 'red', new CtxClock());
+    hours.draw(Math.PI / 3);
+    let minute = new Arrow(0.8, 8, 'green', new CtxClock());
+    minute.draw(Math.PI / 2);
+    let second = new Arrow(0.9, 4, 'black', new CtxClock());
+    second.draw(Math.PI * 2);
     
-    canvasTask1Img.clearRect(50, 50, 550, 1);// ластик
-
-    canvasTask1Img.arc(100, 100, 75, 0, 2*Math.PI);
-    canvasTask1Img.stroke();
-
-    canvasTask1Img.beginPath();
-    canvasTask1Img.moveTo(300, 300);
-    canvasTask1Img.arc(300, 300, 100, (Math.PI/180)*0, (Math.PI/180)*320);
-    canvasTask1Img.fill();
-    canvasTask1Img.stroke();
-
-    canvasTask1Img.clearRect(0, 0, CanvasTask1.getBoundingClientRect().width, CanvasTask1.getBoundingClientRect().height);
-    let clock = new Clock(5, 180, 'white', 'red', canvasTask1Img);
-    clock.draw();
-
-    let canvasFrame = document.querySelector('.canvas__frame');
-    let frame = new CanvasFrame(canvasFrame);
-    let data = new Date();
     
 })
-class Clock {
-    constructor(border, r, color, colorBorder, ctx) {
+
+class ClockFace {
+    constructor(border, color, colorBorder, fontSize, createCanvas) {
+        this.createCanvas = createCanvas;
+        this.ctx = this.createCanvas.ctx;
+        this.center = {
+            x: this.createCanvas.width / 2,
+            y: this.createCanvas.height / 2,
+        };
+        this.fontSize = fontSize;
         this.border = border;
-        this.r = r;
-        this.center = border/2 + r;
+        this.r = (this.center.x > this.center.y ? this.center.y : this.center.x);
         this.color = color;
         this.colorBorder = colorBorder;
-        this.ctx = ctx;
+
+        this.draw = this.draw();
+        
     }
-    face(ctx) {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.center, this.center, this.r, 0, 2*Math.PI);
-        ctx.closePath()
-        ctx.strokeStyle = this.colorBorder;
-        ctx.lineWidth = this.border;
-        ctx.stroke();
-        ctx.fill();
+    face() {
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.colorBorder;
+        this.ctx.arc(this.center.x, this.center.y, this.r, 0, 2 * Math.PI);
+        this.ctx.fill();
+
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.color;
+        this.ctx.arc(this.center.x, this.center.y, this.r-this.border, 0, 2 * Math.PI);
+        this.ctx.fill();
     }
-    hours(ctx, fotSize) {
-        ctx.lineWidth = 1;
-        for (let i=4; i<16; i++) {
-            let x = Math.cos((Math.PI/180)*((i-3)*30))*(this.r-fotSize/2-10);
-            let y = Math.sin((Math.PI/180)*((i-3)*30))*(this.r-fotSize/2-10)+fotSize/3;
+    hours() {
+        this.ctx.beginPath();
+        
+        for (let i = 4; i < 16; i++) {           
+            let x = Math.cos((Math.PI / 180) * ((i - 3) * 30)) * (this.r - this.fontSize / 2 - (this.border+5));
+            let y = Math.sin((Math.PI / 180) * ((i - 3) * 30)) * (this.r - this.fontSize / 2 - (this.border+5)) + this.fontSize / 3;
+            console.log(this.r);
             let a = i;
             if (i > 12) a = i - 12;
-            ctx.font = `bold ${fotSize}px serif`;
-            ctx.textAlign = "center";
-            ctx.fillStyle = this.colorBorder;
-            ctx.fillText(a,this.center+x,this.center+y);
+            this.ctx.textAlign = "center";
+            this.ctx.font = `bold ${this.fontSize}px serif`;
+            this.ctx.fillStyle = "red";
+            this.ctx.fillText(a, this.center.x + x, this.center.y + y);
         }
     }
-    arrowSecond() {
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'yellow'
-        this.ctx.rect(this.center - 1, this.center + 25, 2, -this.r + 20);
-        this.ctx.fill();
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'yellow'
-        this.ctx.arc(this.center, this.center, 5, 0, 2 * Math.PI);
-        this.ctx.fill();
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'black'
-        this.ctx.arc(this.center, this.center, 2, 0, 2 * Math.PI);
-        this.ctx.fill();
-
-    }
-    arrowMinute() {
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'green'
-        this.ctx.rect(this.center - 3, this.center + 15, 6, -this.r + 40);
-        this.ctx.fill();
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'green'
-        this.ctx.arc(this.center, this.center, 7, 0, 2 * Math.PI);
-        this.ctx.fill();
-        this.ctx.beginPath();
-    }
-    arrowHour() {
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'black'
-        this.ctx.rect(this.center - 5, this.center + 10, 10, -this.r + 50);
-        this.ctx.fill();
-        this.ctx.beginPath();
-        this.ctx.fillStyle = 'black'
-        this.ctx.arc(this.center, this.center, 10, 0, 2 * Math.PI);
-        this.ctx.fill();
-        this.ctx.beginPath();
-    }
     draw() {
-        this.face(this.ctx);
-        this.hours(this.ctx, 40);
-        this.arrowHour();
-        this.arrowMinute();
-        this.arrowSecond();
+        this.face();
+        this.hours();
     }
 }
-
-const CanvasUtils = {
-    clear(ctx) {
-        ctx.clearRect(0, 0, 900, 400);
+class Ctx {
+    constructor(width, height, selector) {
+        this.width = width;
+        this.height = height;
+        this.selector = selector;
+        this.ctx = this.create();
+    }
+    create() {
+        let element = document.querySelector(this.selector);
+        let canvas = document.createElement('canvas');
+        canvas.style.position = 'absolute';
+        canvas.style.top = '0px';
+        canvas.style.left = '0px';
+        canvas.width = this.width;
+        canvas.height = this.height;
+        canvas.color = 'red';
+        element.append(canvas);
+        return canvas.getContext('2d');
     }
 }
-
-
-class Circle {
-    constructor(x, y, r, color) {
-        this.x = x;
-        this.y = y;
-        this.r = r;
+class CtxClock {
+    constructor(width, height, selector) {
+        this.width = 400;
+        this.height = 400;
+        this.selector = '.task2';
+        this.ctx = this.create();
+    }
+    create() {
+        let element = document.querySelector(this.selector);
+        let canvas = document.createElement('canvas');
+        canvas.style.position = 'absolute';
+        canvas.style.top = '0px';
+        canvas.style.left = '0px';
+        canvas.width = this.width;
+        canvas.height = this.height;
+        canvas.color = 'red';
+        element.append(canvas);
+        return canvas.getContext('2d');
+    }
+}
+class Arrow {
+    constructor(lenght, width, color, createCanvas) {
+        this.createCanvas = createCanvas;
+        this.ctx = this.createCanvas.ctx;
+        this.center = {
+            x: this.createCanvas.width / 2,
+            y: this.createCanvas.height / 2,
+        };
         this.color = color;
+        this.lenght = lenght * (this.center.x > this.center.y ? this.center.y : this.center.x);
+        this.width = width;
     }
-    draw(ctx) {
-        ctx.fillStyle = this.color;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
-        ctx.closePath();
-        ctx.fill();
+    draw(rortate) {
+        this.ctx.translate(this.center.x, this.center.y);
+        this.ctx.rotate(rortate);
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.color;
+        //this.ctx.rect(this.center.x - this.width / 2, this.center.y, this.width, -this.lenght);
+        this.ctx.rect(-this.width / 2, 0, this.width, -this.lenght);
+        this.ctx.fill();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.color;
+        //this.ctx.arc(this.center.x, this.center.y, this.width*1.5, 0, 2 * Math.PI);
+        this.ctx.arc(0, 0, this.width * 1.2, 0, 2 * Math.PI);
+        this.ctx.fill();
+        this.ctx.beginPath();        
     }
 }
-
 
 class CanvasFrame {
-    constructor(canvas) {
+    constructor(canvas, border) {
+        this.border = border;
         this.ctx = canvas.getContext('2d');
         this.width = canvas.getBoundingClientRect().width;
         this.height = canvas.getBoundingClientRect().height;  
-        this.circle = new Circle(100, 100, 50, 'blue');
-        this.clock = new Clock(2, 196, 'while', 'red', this.ctx);        
+        //this.circle = new Circle(100, 100, 50, 'blue');
+        this.clock = new Clock(this.border, this.width/2, 'RGBA(244,216,147,1)', 'red', this.ctx);
+        //this.arrowSecond = new ArrowSecond(2, 'yellow', this.ctx);
         this.startAnimation();
         this.canvas = canvas;
+
+        //this.secondArrow = new ArrowSecond(4, 'black', new CtxClock());
+        
     }
     startAnimation() {
         requestAnimationFrame(t => {
             if(!this.startAnimationTime) this.startAnimationTime = t;
             this.draw(t - this.startAnimationTime);
             this.startAnimation();
-            
         })
     }
+
     draw(time) {       
-        let currentPosition = time * this.width/20000;
+        let currentPosition = time * this.width/60000;
         if(currentPosition <= this.width) {
-            this.circle.x = currentPosition-this.circle.r;
+            //this.circle.x = currentPosition-this.circle.r;
         }
         else {
             this.clear();
             this.startAnimationTime = 0;
         }
-        this.clear();
-        this.circle.draw(this.ctx);
-        this.clock.draw();
         
+        this.clock.draw();
+        this.secondArrow.draw(45);
+        this.clear();
     }
     clear() {
         this.ctx.clearRect(0, 0, this.width, this.height);
